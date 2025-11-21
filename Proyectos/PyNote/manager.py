cf33bd9090b0f2335a99113e175bda07846507cd
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-11-21 01:45:52 -0500

    feat: agregar funciones para obtener todas las notas, obtener nota por título y borrar nota por id

--- Proyectos/PyNote/manager.py
+++ Proyectos/PyNote/manager.py
@@ -11,14 +11,28 @@ from datetime import date
 
 notes : list = []
 
-def new_id()-> uuid.UUID :
-  return uuid.uuid4()
+def new_id()-> str :
+  return str(uuid.uuid4())
 
 def create_note(title : str, body :str, date_str : str = None) :
-  if date_str == None :
+  if date_str is None :
     date_str = str(date.today())
   notes.append(
     {"id" : new_id(),
      "title" : title,
      "date" : date_str,
      "body" : body})
+
+def get_all_notes() -> list:
+  return notes
+    
+def get_note_by_title(title : str) -> list :
+  return list(filter(lambda n: n["title"] == title, notes))
+
+def delete_note(id : str) -> bool :
+  for n in notes:
+    if n["id"] == id:
+      notes.remove(n)
+      return True
+  return False
+  
