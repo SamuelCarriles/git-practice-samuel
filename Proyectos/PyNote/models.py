d30595e37a76206f60f0396b64dd53093451cde8
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-11-23 16:33:23 -0500

    format: Cambiar id por note_id para no sobrescribir la función de Python llamada id

--- Proyectos/PyNote/models.py
+++ Proyectos/PyNote/models.py
@@ -3,14 +3,14 @@ from typing import Dict
 
 @dataclass
 class Note:
-  id : str
+  note_id : str
   title : str
   date : str
   body : str
   
   def to_dict(self)-> Dict[str,str]:
     return {
-      'id' : self.id,
+      'note_id' : self.note_id,
       'title' : self.title,
       'date' : self.date,
       'body' : self.body
--- Proyectos/PyNote/repository.py
+++ Proyectos/PyNote/repository.py
@@ -1,11 +1,3 @@
-'''
-Notes structure:
-
-{"id" : "current id",
-"title" : "title example",
-"date" : "last modified date",
-"body" : "full text"}
-'''
 import uuid
 from datetime import date
 from database import get_db
@@ -16,7 +8,7 @@ def new_id()-> str :
 
 def row_to_note(row) -> Note :
   return Note(
-    id=row['id'],
+    note_id=row['note_id'],
     title=row['title'],
     date=row['date'],
     body=row['body']
@@ -25,47 +17,65 @@ def row_to_note(row) -> Note :
 def create_note(title : str, body :str, date_str : str = None) -> Note:
   if date_str is None :
     date_str = str(date.today())  
-  return Note(id=new_id(),title=title,date=date_str,body=body)
+  
+  return Note(
+    note_id=new_id(),
+    title=title,
+    date=date_str,
+    body=body
+    )
 
 def save_note(note: Note) -> str:
   with get_db() as conn:
       conn.execute(
-          'INSERT INTO notes (id, title, date, body) VALUES (?, ?, ?, ?)',
-          (note.id, note.title, note.date, note.body)
+          'INSERT INTO notes (note_id, title, date, body) VALUES (?, ?, ?, ?)',
+          (note.note_id, note.title, note.date, note.body)
       )
-  return note.id  
+  return note.note_id  
 
 def get_all_notes() -> list[Note] :
   with get_db() as conn:
     rows = conn.execute('SELECT * FROM notes').fetchall()
   return [row_to_note(r) for r in rows]
-    
+ 
+def get_note_by_id(note_id : str) -> Note | None :
+  with get_db() as conn:
+    row = conn.execute(
+      'SELECT * FROM notes WHERE note_id = ?', 
+      (note_id,)
+      ).fetchone()
+  
+  if row is None:
+    return None
+  
+  return row_to_note(row)
+   
 def get_note_by_title(title : str) -> list[Note] :
   with get_db() as conn:
     rows = conn.execute('SELECT * FROM notes WHERE title = ?', title).fetchall()
     return [row_to_note(r) for r in rows]
     
-def delete_note(id : str) -> bool :
+def delete_note(note_id : str) -> bool :
   with get_db() as conn:
-    cursor = conn.execute('DELETE FROM notes WHERE id = ?', (id,))
+    cursor = conn.execute('DELETE FROM notes WHERE note_id = ?', (note_id,))
   return cursor.rowcount > 0
 
-def update_note(id : str, title : str = None, body : str = None) -> bool :
+def update_note(note_id : str, title : str = None, body : str = None) -> bool :
   with get_db() as conn:
     if title is not None and body is not None:
       cursor = conn.execute(
-        'UPDATE notes SET title = ?, body = ?, date = ? WHERE id = ?',
-        (title, body, str(date.today()), id)
+        'UPDATE notes SET title = ?, body = ?, date = ? WHERE note_id = ?',
+        (title, body, str(date.today()), note_id)
         )
     elif title is not None:
       cursor = conn.execute(
-        'UPDATE notes SET title = ?, date = ? WHERE id = ?',
-        (title, str(date.today()), id)
+        'UPDATE notes SET title = ?, date = ? WHERE note_id = ?',
+        (title, str(date.today()), note_id)
         )
     elif body is not None:
       cursor = conn.execute(
-        'UPDATE notes SET body = ?, date = ? WHERE id = ?',
-        (body, str(date.today()), id)
+        'UPDATE notes SET body = ?, date = ? WHERE note_id = ?',
+        (body, str(date.today()), note_id)
       )
     else:
       return False
