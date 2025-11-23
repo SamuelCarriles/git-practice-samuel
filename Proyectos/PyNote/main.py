3b913006f83d02f5193c76c675c405b022014993
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-11-23 18:17:11 -0500

    feat: endpoints creados, API finalizada

--- Proyectos/PyNote/main.py
+++ Proyectos/PyNote/main.py
@@ -44,3 +44,47 @@ def create_note_endpoint(payload : NoteCreate):
         date_str=payload.date
     )
 
+# Obtener todas las notas
+@app.get('/notes', response_model=List[NoteResponse])
+def list_notes_endpoint():
+  return get_all_notes()
+
+# Obtener nota específica por id
+@app.get('/notes/{note_id}',response_model=NoteResponse)
+def get_note_endpoint(note_id : str):
+  note = get_note_by_id(note_id)
+  
+  if not note:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail='Note not found'
+    )
+  return note
+
+@app.put('/notes/{note_id}',response_model=NoteResponse)
+def update_note_endpoint(note_id : str, payload : NoteUpdate):
+  success = update_note(note_id, payload.title, payload.body)
+  
+  if not success:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail='Note not found'
+    )
+  return get_note_by_id(note_id)
+
+@app.delete('/notes/{note_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_note_endpoint(note_id : str):
+  success = delete_note(note_id)
+  
+  if not success:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail='Note not found'
+    )
+  return None
+
+@app.get('/search',response_model=List[NoteResponse])
+def search_endpoint(q : str):
+  if not q.strip():
+    return []
+  return search_notes(q)
